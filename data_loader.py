0d293f02c82e81d8f297745a65dfe6906ea5d041
yizhou <yizhouxu@bhn-server>
2023-12-02 09:07:04 +0000

    bug fix

--- data_loader.py
+++ data_loader.py
@@ -114,7 +114,8 @@ class NoisyClothing1M(Dataset):
         self.target_transform = target_transform
     
     def __getitem__(self, idx):
-        idx = idx.item()
+        if type(idx) is torch.Tensor:
+            idx = idx.item()
         img_path = self.img_dir / self.img_labels.iloc[idx, 0]
         image = Image.open(str(img_path))
         label = self.img_labels.iloc[idx, 1]
