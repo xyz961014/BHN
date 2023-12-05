1e73ef438bcdc8306595467bde46ac1aa5c5a58a
yizhou <yizhouxu@bhn-server>
2023-12-05 13:30:38 +0000

    transforms

--- train.py
+++ train.py
@@ -173,8 +173,11 @@ def main(args):
         train_transform = transforms.Compose([
             transforms.Resize(256),  # Resize the image to 256 x 256
             transforms.CenterCrop(224),  # Crop the middle 224 x 224
+            #transforms.RandomResizedCrop(224, scale=(0.2, 1.)),  # Crop the middle 224 x 224
             transforms.RandomCrop(size=224, padding=32),
             transforms.RandomHorizontalFlip(),
+            #transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            #transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet stats
         ])
